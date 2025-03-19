728371d57ad6365321fb29ddd7a426409d31d746
Alfarabx <alfarabi1074@gmail.com>
2025-03-19 16:15:28 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import FooterMobile from "@/src/components/footer";
 export default function Home() {
   return (
     <main>
+      <meta name="google-site-verification" content="At6_nCIFy-Yfjt4tVp9ta_xKUpZkguongM6imsKF_Oc" />
       <HeaderComponent></HeaderComponent>
       <IndexComponent></IndexComponent>
       <FooterMobile></FooterMobile>
